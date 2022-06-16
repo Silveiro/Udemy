7d05f00919d45498f7e718f41a4696b2553424b5
justo1 <justo1@MacBook-Pro-de-justo.local>
2022-06-16 00:18:23 -0400

    avances Udemy veterinaria

--- Pacientes-veterinaria/js.js
+++ Pacientes-veterinaria/js.js
@@ -1,8 +1,7 @@
 //const
 const formulario = document.querySelector("#formulario");
 const botonformulario = document.querySelector("#botonformulario");
-
-
+var arrcitas = [];
 
 //Event Listeners
 eventListeners();
@@ -14,7 +13,7 @@ function eventListeners() {
 //Clases
 
 class Cita {
-    constructor(id,nombreM, nombreP, hora, notas,celular, dia) {
+    constructor(id, nombreM, nombreP, hora, notas, celular, dia) {
         this.id = id;
         this.nombreM = nombreM;
         this.nombreP = nombreP;
@@ -41,14 +40,14 @@ function validarDatos() {
     var x = new Date();
     var y = new Date();
     var yy = y.getTime();
-    var nuevaCita = new Cita(yy,nombreMascota,nombrePropietario,hora,sintomas);
+    var nuevaCita = new Cita(yy, nombreMascota, nombrePropietario, hora, sintomas);
 
-    
+
 
     if (tel === 0) {
         telefono.classList.add("red-border");
         alertar("Telefono NO valido");
-        return("Telefono NO valido")
+        return ("Telefono NO valido")
     } else {
         telefono.classList.remove("red-border");
         console.log("Telefono SI valido");
@@ -59,17 +58,19 @@ function validarDatos() {
     if (fecha1 < x) {
         fecha.classList.add("red-border");
         alertar("Fecha NO valida");
-        return("Fecha NO valida")
+        return ("Fecha NO valida")
     } else {
         fecha.classList.remove("red-border");
         console.log("Fecha SI valida");
         nuevaCita.fecha1 = fecha1;
 
     }
-    var nuevaCita = new Cita(yy,nombreMascota,nombrePropietario,hora,sintomas,tel,fecha1);
-    console.log(nuevaCita);
-    mostrarCitas(nuevaCita);
+    var nuevaCita = new Cita(yy, nombreMascota, nombrePropietario, hora, sintomas, tel, fecha1);
+    arrcitas.push(nuevaCita);
+    console.log(arrcitas);
+    mostrarCitas(arrcitas);
     formulario.reset();
+
 }
 
 //Alertar Errores
@@ -81,10 +82,17 @@ function alertar(mensaje) {
     }, 3000);
 }
 
-function mostrarCitas(obj){
-    const {id, nombreM, nombreP, hora, notas, celular,dia} = obj;
-    document.getElementById("der").innerHTML = nombreM + "<br>" + dia + "<br>" + hora + "<br>" + notas;
+function mostrarCitas(arreglo) {
+    const para = document.createElement("div");
+
+    for (var i = 0; i < arreglo.length; i++) {
+        console.log(i);
+        while (para.firstChild) {
+            para.removeChild(para.firstChild);
+        }
+        para.innerHTML ="<p>"+arreglo[i].id+"</p>"+"<p>"+arreglo[i].nombreM+"</p>"+"<p>"+arreglo[i].nombreP+"</p>"+"<p>"+"+"+arreglo[i].celular+"</p>"+"<p>"+arreglo[i].dia+"</p>"+arreglo[i].hora+"<p>"+arreglo[i].notas+"</p>"+"<button>"+"Eliminar"+"</button>";
+        document.getElementById("der").appendChild(para);
+    }
 
 }
-
 
